078928d351d1d1a898cc5777d4c61094c0347778
Muhammad Muneer <MdMuneer310@gmail.com>
2023-07-06 19:03:15 +0530

    added empty state for collection page

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -10,6 +10,10 @@ export default function Home() {
     return b._creationTime - a._creationTime;
   });
 
+  const emptyState = !saveSketchMutation
+    ? "loading Sketches..."
+    : "No recent sketches :(";
+
   return (
     <main className="flex min-h-screen flex-col items-center  mt-6">
       <div className="flex items-center mb-6 w-3/4">
@@ -17,19 +21,23 @@ export default function Home() {
         <div className=" mx-4 font-bold">All Collections</div>
         <div className="h-[2px] flex-1 bg-[#EEDC82]"></div>
       </div>
-      <div className="grid grid-cols-4 gap-4">
-        {sortedSketches.map((sketch) => (
-          <Image
-            src={sketch.result}
-            width="256"
-            height="256"
-            alt={sketch.result}
-            key={sketch.id}
-            placeholder="blur"
-            blurDataURL="/Assets/Blur.webp"
-          />
-        ))}
-      </div>
+      {sortedSketches?.length > 0 ? (
+        <div className="grid grid-cols-4 gap-4">
+          {sortedSketches.map((sketch) => (
+            <Image
+              src={sketch.result}
+              width="256"
+              height="256"
+              alt={sketch.result}
+              key={sketch.id}
+              placeholder="blur"
+              blurDataURL="/Assets/Blur.webp"
+            />
+          ))}
+        </div>
+      ) : (
+        <span className="w-full text-center font-medium">{emptyState}</span>
+      )}
     </main>
   );
 }
